e5385d6975217edc540cb71a21f51597c7236cc4
Charles Seymour <charlesseymour@yahoo.com>
2019-07-29 17:24:51 -0500

    added functions

--- inc/functions.php
+++ inc/functions.php
@@ -1,3 +1,5 @@
+<?php
+
 // PHP - Random Quote Generator
 
 // Create the Multidimensional array of quote elements and name it quotes
@@ -13,29 +15,48 @@ $quotes = [
     "source" => "Shelby Foote"
   ],
   [
-    "quote" => “I had found my religion: nothing seemed more important to me than a book. I saw the library as a temple.”,
+    "quote" => "I had found my religion: nothing seemed more important to me than a book. I saw the library as a temple.",
     "source" => "Jean Paul Sartre",
-    "citation" => "The Words".
+    "citation" => "The Words",
     "year" => 1963
   ],
   [
-    "quote" => "When I have a house of my own, I shall be miserable if I have not an excellent library.”,
+    "quote" => "When I have a house of my own, I shall be miserable if I have not an excellent library.",
     "source" => "Jane Austen",
     "citation" => "Pride and Prejudice",
     "year" => 1813
-  ].
+  ],
   [
-    "quote" => “Me, poor man, my library /n Was dukedom large enough.”,
+    "quote" => "Me, poor man, my library was dukedom large enough.",
     "source" => "William Shakespeare",
     "citation" => "The Tempest",
     "year" => 1611
   ]
+];
 
-
-]
 
 // Create the getRandomQuuote function and name it getRandomQuote
 
-
+function getRandomQuote($array) {
+  $index = rand(0, count($array) - 1);
+  return $array[$index];
+}
 
 // Create the printQuote funtion and name it printQuote
+
+function printQuote($array) {
+  $quote = getRandomQuote($array);
+  $html = "";
+  $html .= "<p class=\"quote\">$quote[quote]</p>";
+  $html .= "<p class=\"source\">$quote[source]";
+  if (isset($quote["citation"])) {
+    $html .= "<span class=\"citation\">$quote[citation]</span>";
+  };
+  if (isset($quote["year"])) {
+    $html .= "<span class=\"year\">$quote[year]</span>";
+  };
+  $html .= "</p>";
+  echo($html);
+}
+
+?>
