72025ef3f73603260dc87ab747b91a35217c179f
Charles Seymour <charlesseymour@yahoo.com>
2019-07-29 11:52:22 -0500

    Added quotes

--- inc/functions.php
+++ inc/functions.php
@@ -3,7 +3,36 @@
 // Create the Multidimensional array of quote elements and name it quotes
 // Each inner array element should be an associative array
 
+$quotes = [
+  [
+    "quote" => "I have always imagined that Paradise will be a kind of library.",
+    "source" => "Jorge Luis Borges"
+  ],
+  [
+    "quote" => "A university is just a group of buildings gathered around a library.",
+    "source" => "Shelby Foote"
+  ],
+  [
+    "quote" => “I had found my religion: nothing seemed more important to me than a book. I saw the library as a temple.”,
+    "source" => "Jean Paul Sartre",
+    "citation" => "The Words".
+    "year" => 1963
+  ],
+  [
+    "quote" => "When I have a house of my own, I shall be miserable if I have not an excellent library.”,
+    "source" => "Jane Austen",
+    "citation" => "Pride and Prejudice",
+    "year" => 1813
+  ].
+  [
+    "quote" => “Me, poor man, my library /n Was dukedom large enough.”,
+    "source" => "William Shakespeare",
+    "citation" => "The Tempest",
+    "year" => 1611
+  ]
 
+
+]
 
 // Create the getRandomQuuote function and name it getRandomQuote
 
